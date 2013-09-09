5ef61c2cd8851ab3fb69d4012bd5ed2a3cd1c5f7
Avenel <martin.briewig@googlemail.com>
2013-09-09 12:57:19 +0200

    Added feature: get List of News from Server.
    I have written a Ruby on Rails Server, which provides the News data.
    The App access this data, creates its own new items and displays a
    celllist.

--- src/main/java/de/hska/iwi/mgwt/demo/client/model/News.java
+++ src/main/java/de/hska/iwi/mgwt/demo/client/model/News.java
@@ -2,6 +2,9 @@ package de.hska.iwi.mgwt.demo.client.model;
 
 import java.util.Date;
 
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.json.client.JSONObject;
+
 public class News {
 	
 	private String id;
@@ -44,4 +47,20 @@ public class News {
 	public void setDate(Date date) {
 		this.date = date;
 	}
+	
+	public static News fromJson(JSONObject value) {
+		News news = new News();
+		
+		news.id = value.get("id").isNumber().toString();
+		news.title = value.get("title").isString().stringValue();
+		news.content = value.get("content").isString().stringValue();
+		news.organisation = value.get("organisation").isString().stringValue();
+
+		DateTimeFormat fmt = DateTimeFormat.getFormat("yyyy-MM-d");
+		String newsDate = value.get("date").toString();
+		newsDate = newsDate.replace('\"', ' ').trim();
+		news.date = fmt.parse(newsDate);
+		return news;
+	}
+
 }
--- src/main/java/de/hska/iwi/mgwt/demo/client/model/NewsUtility.java
+++ src/main/java/de/hska/iwi/mgwt/demo/client/model/NewsUtility.java
@@ -5,75 +5,99 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.http.client.URL;
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONString;
+import com.google.gwt.json.client.JSONValue;
+import com.google.gwt.user.client.Window;
+
 public class NewsUtility {
 
 	private static List<News> newsList;
 	private static HashMap<String, News> newsMap;
-	
+
 	public static List<News> getSortedNewsList() {
 		init();
 		return newsList;
 	}
-	
+
 	public static News getNewsById(String id) {
-		init();	
+		init();
 		return newsMap.get(id);
 	}
-	
-	
+
 	/**
 	 * Initialize fixtures, dummy content
 	 */
 	private static void init() {
 		if (newsList == null) {
 			newsList = new ArrayList<News>();
-			
-			News n = new News();
-			n.setId("1");
-			n.setTitle("Neuer Koch");
-			n.setContent("insert content here");
-			n.setOrganisation("[Mensa]");
-			n.setDate(new Date());
-			newsList.add(n);
-			
-			n = new News();
-			n.setId("2");
-			n.setTitle("Ehrung Thilo Enters");
-			n.setContent("insert content here");
-			n.setOrganisation("[IWI]");
-			n.setDate(new Date());
-			newsList.add(n);
-			
-			n = new News();
-			n.setId("3");
-			n.setTitle("Kanzlerin wird 50");
-			n.setContent("insert content here");
-			n.setOrganisation("[R]");
-			n.setDate(new Date());
-			newsList.add(n);
-			
-			n = new News();
-			n.setId("4");
-			n.setTitle("Werden wir alle duemmer?");
-			n.setContent("insert content here");
-			n.setOrganisation("[IWI]");
-			n.setDate(new Date());
-			newsList.add(n);
-			
-			n = new News();
-			n.setId("5");
-			n.setTitle("Reinigungspersonal vermisst Tasche");
-			n.setContent("insert content here");
-			n.setOrganisation("[Mensa]");
-			n.setDate(new Date());
-			newsList.add(n);
+
+			String url = "http://localhost:3000/news?format=json";
+			RequestBuilder builder = new RequestBuilder(RequestBuilder.GET,
+					URL.encode(url));
+
+			try {
+				Request request = builder.sendRequest(null,
+				new RequestCallback() {
+					public void onError(Request request,
+							Throwable exception) {
+						// Couldn't connect to server (could be timeout,
+						// SOP violation, etc.)
+						exception.printStackTrace();
+					}
+
+					public void onResponseReceived(Request request,
+							Response response) {
+						if (200 == response.getStatusCode()) {
+							// Process the response in
+							System.out.println(response.getText());
+
+							JSONValue jsonValue;
+							JSONArray jsonArray;
+
+							jsonValue = JSONParser.parseStrict(response
+									.getText());
+
+							jsonArray = jsonValue.isArray();
+							if (jsonArray == null) {
+								Window.alert("Error parsing the JSON (isArray)");
+							}
+							
+							jsonValue = jsonArray.get(0);
+							if (jsonValue.isObject().get("id").isNumber() == null) {
+								Window.alert("Error parsing the JSON (isNumber)");
+							}
+
+							for (int i=0; i < jsonArray.size(); i++) {
+								News news = News.fromJson(jsonArray.get(i).isObject());
+								newsList.add(news);
+							}
+						} else {
+							// Handle the error. Can get the status text
+							// from response.getStatusText()
+							System.out.println("Error: :("
+									+ response.getStatusCode());
+						}
+					}
+				});
+			} catch (RequestException e) {
+				e.printStackTrace();
+			}
 		}
-		
+
 		newsMap = new HashMap<String, News>();
-		
-		for(News n : newsList) {
+
+		for (News n : newsList) {
 			newsMap.put(n.getId(), n);
 		}
 	}
-	
+
 }
