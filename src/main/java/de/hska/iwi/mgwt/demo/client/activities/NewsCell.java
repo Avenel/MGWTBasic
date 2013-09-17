d35a61fde5b1d5edfa8d2aae8bf696dcef35c937
Avenel <martin.briewig@googlemail.com>
2013-09-17 09:46:45 +0200

    replaced organisation with date in cell rows.

--- src/main/java/de/hska/iwi/mgwt/demo/client/activities/NewsCell.java
+++ src/main/java/de/hska/iwi/mgwt/demo/client/activities/NewsCell.java
@@ -1,6 +1,7 @@
 package de.hska.iwi.mgwt.demo.client.activities;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.safehtml.client.SafeHtmlTemplates;
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
@@ -14,12 +15,13 @@ public class NewsCell implements Cell<News> {
 	
 	public interface Template extends SafeHtmlTemplates {
 		@SafeHtmlTemplates.Template("<div style='float:left'><b>{0}</b></div><div style='float:right'><i>{1}</i></div>")
-		SafeHtml content(String cellContents, String organisation);
+		SafeHtml content(String cellContents, String date);
 	}
 	
 	@Override
 	public void render(SafeHtmlBuilder safeHtmlBuilder, News model) {
-		SafeHtml content = TEMPLATE.content(model.getTitle(), model.getOrganisation());
+		DateTimeFormat fmt = DateTimeFormat.getFormat("dd.MM");
+		SafeHtml content = TEMPLATE.content(model.getTitle(), fmt.format(model.getDate()));
 		
 		safeHtmlBuilder.append(content);
 	}
