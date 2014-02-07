828ecd5d2b1a51d68315c0419493bdd62d990878
Avenel <martin.briewig@googlemail.com>
2014-02-07 09:35:42 +0100

    fixed missing places in jsontoplaceMapper. Now it works :)

--- src/main/java/de/hska/iwi/mgwt/demo/client/model/JSONToPlaceMapper.java
+++ src/main/java/de/hska/iwi/mgwt/demo/client/model/JSONToPlaceMapper.java
@@ -1,11 +1,16 @@
 package de.hska.iwi.mgwt.demo.client.model;
 
-import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONValue;
 import com.google.gwt.place.shared.Place;
 
 import de.hska.iwi.mgwt.demo.client.activities.home.HomePlace;
+import de.hska.iwi.mgwt.demo.client.activities.lecture.LecturePlace;
+import de.hska.iwi.mgwt.demo.client.activities.mensa.MensaPlace;
+import de.hska.iwi.mgwt.demo.client.activities.news.NewsPlace;
 import de.hska.iwi.mgwt.demo.client.activities.processes.ProcessDetailPlace;
+import de.hska.iwi.mgwt.demo.client.activities.processes.StudentPlace;
+import de.hska.iwi.mgwt.demo.client.activities.processes.seminar.ProcessSeminarPlace;
+import de.hska.iwi.mgwt.demo.client.activities.settings.SettingsPlace;
 
 /**
  * Maps JSON to Places.
@@ -20,20 +25,11 @@ public class JSONToPlaceMapper {
 	 * @return Place targetPlace
 	 */
 	public static Place toPlace(JSONValue jsonValue) {
-		JSONArray jsonArray = null;
-		
-		jsonArray = jsonValue.isArray();
-		if (jsonArray == null) {
-			// Error, return to HomePlace
-			return new HomePlace();
-		}
-		
-		jsonValue = jsonArray.get(0);
-		if (jsonValue.isObject().get("class").isString() == null) {
+		if (jsonValue.isObject().get("className").isString() == null) {
 			// Error, return to HomePlace
 			return new HomePlace();
 		} else {
-			String className = jsonValue.isObject().get("class").isString().stringValue(); 
+			String className = jsonValue.isObject().get("className").isString().stringValue(); 
 			
 			// ProcessDetailPlace
 			if (className.compareTo("ProcessDetailPlace") == 0) {
@@ -41,8 +37,36 @@ public class JSONToPlaceMapper {
 					// Error, return to HomePlace
 					return new HomePlace();
 				} else {
-					return new ProcessDetailPlace(jsonValue.isObject().get("id").isString().stringValue());
+					return new ProcessDetailPlace(jsonValue.isObject().get("id").isNumber().toString());
 				}
+			}
+			
+			if (className.compareTo("HomePlace") == 0) {
+				return new HomePlace();
+			}
+			
+			if (className.compareTo("LecturePlace") == 0) {
+				return new LecturePlace();
+			}
+			
+			if (className.compareTo("MensaPlace") == 0) {
+				return new MensaPlace();
+			}
+			
+			if (className.compareTo("NewsPlace") == 0) {
+				return new NewsPlace();
+			}
+			
+			if (className.compareTo("StudentPlace") == 0) {
+				return new StudentPlace();
+			}
+			
+			if (className.compareTo("ProcessSeminarPlace") == 0) {
+				return new ProcessSeminarPlace();
+			}
+			
+			if (className.compareTo("SettingsPlace") == 0) {
+				return new SettingsPlace();
 			}
 		}
 		
