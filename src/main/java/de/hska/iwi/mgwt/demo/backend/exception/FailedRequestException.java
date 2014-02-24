d2580cb0c89c0ee842d3279284c73fcc485b314e
bowdoh <bowdoh@gmail.com>
2014-02-24 23:07:53 +0100

    removed C'tor which comes with Java 7 completely

--- src/main/java/de/hska/iwi/mgwt/demo/backend/exception/FailedRequestException.java
+++ src/main/java/de/hska/iwi/mgwt/demo/backend/exception/FailedRequestException.java
@@ -38,17 +38,17 @@ public class FailedRequestException extends RuntimeException {
 	public FailedRequestException(String message, Throwable cause) {
 		super(message, cause);
 	}
-
-	/**
-	 * C'tor for creating a new FailedRequestException
-	 * @param message detailed message
-	 * @param cause antoher Throwable
-	 * @param enableSuppression whether or not suppression is enabled or disabled
-	 * @param writableStackTrace whether or not the stack trace should be writable
-	 */
-	public FailedRequestException(String message, Throwable cause,
-			boolean enableSuppression, boolean writableStackTrace) {
+//	C'tor comes with Java 7
+//	/**
+//	 * C'tor for creating a new FailedRequestException
+//	 * @param message detailed message
+//	 * @param cause antoher Throwable
+//	 * @param enableSuppression whether or not suppression is enabled or disabled
+//	 * @param writableStackTrace whether or not the stack trace should be writable
+//	 */
+//	public FailedRequestException(String message, Throwable cause,
+//			boolean enableSuppression, boolean writableStackTrace) {
 //		super(message, cause, enableSuppression, writableStackTrace);
-	}
+//	}
 
 }
